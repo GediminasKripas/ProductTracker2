cd55633497752ae0560bc3e5566e23c27a9f55c5
unknown <justnanosprint@gmail.com>
2022-04-06 00:42:38 +0300

    added files

---
+++ ProductWebservice/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProductWebservice.Models
+{
+    public class Product
+    {
+        [JsonIgnore]
+        public string id { get; set; }
+        public string productName { get; set; }
+        public double price { get; set; }
+        public string date { get; set; }
+    }
+}
