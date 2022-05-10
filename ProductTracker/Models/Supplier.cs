79ad3bc7865abf703b15c109839a669d07db913a
unknown <justnanosprint@gmail.com>
2022-05-10 20:51:59 +0300

    added supplier model

---
+++ ProductTracker/Models/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductTracker.Models
+{
+    public class Supplier
+    {
+        public int id { get; set; }
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string number { get; set; }
+        public string email { get; set; }
+    }
+}
